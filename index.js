5c9d62dbdd76cb3655457ea7e1c352e6d335949b
Youngpuffy <levimuturi2@gmail.com>
2025-03-20 11:15:06 +0300

    filters elements in array

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, query){
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+function fuzzyMatch(drivers, query){
+    return drivers.filter(driver => driver.toLowerCase().startsWith( query.toLowerCase()));
+
+}
+function matchName(drivers, query){
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+
+}
